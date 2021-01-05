d7aa1e428e525a08f7c4f3303bef77775419f331
Michael Scoli <mscoli44@gmail.com>
2021-01-04 21:16:30 -0500

    Final Changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,99 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+const employeeList = document.getElementById('employees');
+const wrapper = document.getElementsByClassName('wrapper')[0];
+const search = document.getElementById('mySearch');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+function createCard(data) {
+  let card = document.createElement('div');
+  card.classList.add('card');
+  card.id = data.login.username;
+  card.innerHTML = `<img src="${data.picture.large}">
+  <div class="card-text">
+    <h2 class="name">${data.name.first} ${data.name.last}</h2>
+    <span class="email">${data.email}</span>
+    <span class="city">${data.location.city}</span>
+  </div>`;
+  employeeList.appendChild(card);
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+function createModal(data) {
+  let newModal = document.createElement('div');
+  newModal.classList.add('modal');
+  newModal.innerHTML = `<div class="modal-content">
+    <span class="close">&times;</span>
+    <img src="${data.picture.large}">
+    <div class="arrows">
+      <span class="prev">&#9668;</span>
+      <span class="next">&#9658;</span>
+    </div>
+    <div class="modal-text">
+      <h2>${data.name.first} ${data.name.last}</h2>
+      <p>${data.email}</p>
+      <p class="city">${data.location.city}</p>
+    </div>
+    <div class="modal-contact">
+      <p>${data.cell}</p>
+      <p class="city">${data.location.street}, ${data.location.state} ${data.location.postcode}</p>
+      <p>Birthday: ${data.dob.date.replace(/T.*/,'').split('-').reverse().join('/')}</p>
+    </div>
+  </div>`;
+  wrapper.appendChild(newModal);
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+const url = 'https://randomuser.me/api/?results=12&nat=au';
+fetch(url)
+  .then((resp) => resp.json())
+  .then(function(data) {
+    let employees = data.results;
+    return employees.map(function(employee) {
+      createCard(employee);
+      createModal(employee);
+    });
+  })
+  .catch(function(error) {
+  console.log(error);
+});
+
+// Modal functionality
+
+wrapper.addEventListener('click', (e) => {
+  let card = document.querySelectorAll('.card');
+  let modal = document.querySelectorAll('.modal');
+  let close = document.querySelectorAll('.close');
+  let next = document.querySelectorAll('.next');
+  let prev = document.querySelectorAll('.prev');
+  for (i = 0; i < card.length; i++) {
+    if (e.target === card[i]) {
+      modal[i].style.display = 'block';
+      if (card[0]) {
+        prev[0].style.visibility = 'hidden';
+        if (card[card.length - 1]) {
+          next[next.length - 1].style.visibility = 'hidden';
+        }
+      } 
+    } else if (e.target === modal[i] || e.target === close[i]) {
+      modal[i].style.display = 'none';
+    } else if (e.target === next[i]) {
+      modal[i].style.display = 'none';
+      modal[i+1].style.display = 'block';
+    } else if (e.target === prev[i]) {
+      modal[i].style.display = 'none';
+      modal[i-1].style.display = 'block';
+    }
   }
-}
+});
+
+search.addEventListener('keyup', () => {
+  let searchVal = search.value.toLowerCase();
+  let nameItems = document.querySelectorAll('h2.name'); //get name by nameItems[i].innerText
+  let cards = document.querySelectorAll('.card'); // get id by searching cards[i].id
+
+  for (i = 0; i < cards.length; i++) {
+    let id = cards[i].id;
+    let name = nameItems[i].innerText.toLowerCase();
+    if (id.indexOf(searchVal) > -1 || name.indexOf(searchVal) > -1 ){
+      cards[i].style.display = 'flex';
+    } else {
+      cards[i].style.display = 'none';
+    }
+  }
+});
